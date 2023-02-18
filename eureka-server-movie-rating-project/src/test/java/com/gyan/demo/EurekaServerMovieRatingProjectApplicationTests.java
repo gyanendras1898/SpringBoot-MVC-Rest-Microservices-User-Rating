3221c94f1d650013b065a76faaf06568a31f11ae
gyanendra <gyanendras1898@gmail.com>
2023-02-18 23:41:34 +0530

    Implements Eureka server for movie rating and load balancing

---
+++ eureka-server-movie-rating-project/src/test/java/com/gyan/demo/EurekaServerMovieRatingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMovieRatingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
